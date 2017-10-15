55446c919adb603057b1934363d403df5fbdb466
Cameron Edwards <cameron@cameronedwards.net>
2017-10-14 23:37:22 -0400

    Added gitignore and init-packages

---
+++ init-packages.el
@@ -0,0 +1,20 @@
+(require 'package)
+
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+(setq package-list
+      `(ace-window magit))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
