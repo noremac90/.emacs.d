474f6d587661ec60cb1c3f1ea769f9d3c1eba181
Cameron Edwards <cameron@cameronedwards.net>
2017-10-18 22:09:58 -0400

    added pwoerline to  selected packages

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  '(custom-safe-themes
    (quote
     ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (which-key default-text-scale ace-window magit))))
+ '(package-selected-packages (quote (powerline which-key default-text-scale ace-window magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
